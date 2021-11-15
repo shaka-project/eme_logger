shaka-project/eme_logger

commit 8d09fea648cd41e410878d6acb25081d39f41c7a

fix: Fix log window focus
Without a popup, we can just directly open the window from the
background script.  In this context, we have permission to focus a
window.

Fixes #23

Change-Id: I572c71fb65a9b5ac71e9d6afd1d04cefbe650eed

log-window.js
@@ -267,9 +267,15 @@ function prettyPrint(obj, indentation = '') {
  * Listens for messages from the content script to append a log item to the
  * current frame and log file.
  */
-if (chrome.runtime) {
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const log = request.log;
-    EmeLogWindow.instance.appendLog(log);
-  });
-}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const log = request.log;
+  EmeLogWindow.instance.appendLog(log);
+});
+
+/**
+ * When the extension icon is clicked, open the log window if it doesn't exist,
+ * and bring it to the front otherwise.
+ */
+chrome.browserAction.onClicked.addListener((tab) => {
+  EmeLogWindow.instance.open();
+});

log.html
@@ -15,6 +15,7 @@
 -->
 <html>
   <head>
+    <title>EME Logger</title>
     <script src="log.js"></script>
     <style>
 #clear-button {

popup.html
@@ -1,20 +1,0 @@
-<!--
- Copyright 2015 Google Inc.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
--->
-<html>
-  <head>
-    <script src="popup.js"></script>
-  </head>
-</html>

popup.js
@@ -1,30 +1,0 @@
-/**
- * Copyright 2015 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @fileoverview Code for the "popup" when the extension is clicked.
- */
-
-// When the user clicks the extension icon, open the EME log window.
-document.addEventListener('DOMContentLoaded', () => {
-  const backgroundPage = chrome.extension.getBackgroundPage();
-  const emeLogWindow = backgroundPage.EmeLogWindow.instance;
-
-  // Without a small delay, it opens behind the current window.  Adding the
-  // delay makes it a proper popup that takes focus.
-  setTimeout(() => {
-    emeLogWindow.open();
-    window.close();
-  }, 100);
-});

commit e8ac1fbbda9ccad8b0b4e52ee7b8c4f0b3914bac

test: Fix test failure due to unconditional extension API usage
This test failure was introduced in 8d09fea6.

Change-Id: Ibdce76e43fda310c699070e0355b0b6bb7b6e01a

log-window.js
@@ -264,19 +264,23 @@ function prettyPrint(obj, indentation = '') {
   return obj.toString();
 }
 
-/**
- * Listens for messages from the content script to append a log item to the
- * current frame and log file.
- */
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  const log = request.log;
-  EmeLogWindow.instance.appendLog(log);
-});
+// NOTE: These APIs are not defined in our test environment, but should always
+// be present when this is run as a Chrome extension.
+if (chrome.runtime !== undefined) {
+  /**
+   * Listens for messages from the content script to append a log item to the
+   * current frame and log file.
+   */
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    const log = request.log;
+    EmeLogWindow.instance.appendLog(log);
+  });
 
-/**
- * When the extension icon is clicked, open the log window if it doesn't exist,
- * and bring it to the front otherwise.
- */
-chrome.browserAction.onClicked.addListener((tab) => {
-  EmeLogWindow.instance.open();
-});
+  /**
+   * When the extension icon is clicked, open the log window if it doesn't exist,
+   * and bring it to the front otherwise.
+   */
+  chrome.browserAction.onClicked.addListener((tab) => {
+    EmeLogWindow.instance.open();
+  });
+}